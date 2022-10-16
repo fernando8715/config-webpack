97c1d32175344a76a68b1df3db101ad6f9761379
fernando8715 <fernando8715@hotmail.com>
2022-10-16 15:23:55 -0500

    configuracion webpack

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+//El import hago referencia al archivo js que esta en la carpeta js
+import {saludar} from './js/componentes'; 
+import './styles.css';
+
+const nombre = 'Fernando';
+
+saludar(nombre);
+
+
+
+
+
+
+
+
+
